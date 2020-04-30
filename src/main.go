11c9250fc801f6b99e1601fa3db6e9986efa55e1
gauravkarkal <gauravkarkal@gmail.com>
2020-05-01 01:25:04 +0530

    Added docker

--- src/main.go
+++ src/main.go
@@ -62,7 +62,7 @@ func callPlacementDeptVerification(name string, company string) {
 	if err != nil {
 		print(err)
 	}
-	resp, err := http.Post("http://placementdept-cluster2-jen-ci.devtools-dev.ext.devshift.net/verify-PlacementDept",
+	resp, err := http.Post("http://localhost:8084/verify-PlacementDept",
 		"application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
 		print(err)
@@ -85,7 +85,7 @@ func callprintUsage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	port := "8080"
+	port := "8083"
 	http.HandleFunc("/verify-AcademicDept", callverificationByAcademicDept)
 	http.HandleFunc("/usage", callprintUsage)
 	fmt.Printf("Server listening on localhost:%s\n", port)
